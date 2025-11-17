cf4b9ebed5a7d9dd9cd82462dcae9266292ca24a
Manu Agrawal <agrawalmanu73@gmail.com>
2025-11-17 10:50:07 +0530

    WEB1 Tala

---
+++ Sreyansh online/Locomotive.js
@@ -0,0 +1,4 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'), // [data-scroll-container] iski jagah #main likh do.. nahi to html ma #main ke bagal ma data-scroll-container ye likh do
+    smooth: true
+});
